3db0fc43a139b256a0e68a79d1bdc8fe6ec225d6
Vladimir <99782728+vpostyka@users.noreply.github.com>
2023-04-05 21:39:07 -0700

    added js skills

--- js/index.js
+++ js/index.js
@@ -15,3 +15,24 @@ copyright.innerHTML = 'Vladimir ' + '&copy;' + thisYear;
 
 // Append the copyright element to the footer
 footer.appendChild(copyright);
+
+// List of technical skills
+const skills = ['HTML', 'CSS', 'JavaScript', 'Python', 'SQL'];
+
+// Get the skills section
+const skillsSection = document.getElementById('skills');
+
+// Get the unordered list element
+const skillsList = skillsSection.querySelector('ul');
+
+// Loop through the skills array and add each skill to the list
+for (let i = 0; i < skills.length; i++) {
+  // Create a new list item element
+  const skill = document.createElement('li');
+
+  // Set the inner text of the list item element to the current skill
+  skill.innerText = skills[i];
+
+  // Append the list item element to the unordered list
+  skillsList.appendChild(skill);
+}
